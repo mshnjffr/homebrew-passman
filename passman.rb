ef24c91c81445f7d9eb2c5cac7dd31efabb25653
mshnjffr <moshinrlz@hotmail.co.uk>
2025-07-03 10:30:39 +0100

    Update passman.rb
    Update passman to v1.0.3 - Enhanced responsive design and UI improvements

--- passman.rb
+++ passman.rb
@@ -1,8 +1,8 @@
 class Passman < Formula
   desc "Beautiful terminal password manager built with Go and Bubble Tea"
   homepage "https://github.com/mshnjffr/passman"
-  url "https://github.com/mshnjffr/passman/archive/refs/tags/v1.0.2.tar.gz"
-  sha256 "4ba1fc2bb5e8a2c03115418778c566d7222d0ff0f9b2a2bb61057f308b6e5698"
+  url "https://github.com/mshnjffr/passman/archive/refs/tags/v1.0.3.tar.gz"
+  sha256 "b25e533d21faec1358a8eb2b90cd77b79286cff8af0b7d3c07e58c6d83145d27"
   license "MIT"
 
   depends_on "go" => :build
